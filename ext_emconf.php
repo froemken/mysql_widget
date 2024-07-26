d7eae9f856b5d5dea08f5189510bebb89f286733
Stefan Froemken <froemken@gmail.com>
2024-07-27 00:19:08 +0200

    Require TYPO3 dashboard

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,6 +14,7 @@ $EM_CONF[$_EXTKEY] = [
         'depends' => [
             'php' => '8.1.0-8.3.99',
             'typo3' => '12.4.17-13.4.99',
+            'dashboard' => '12.4.17-13.4.99',
         ],
         'conflicts' => [
         ],
