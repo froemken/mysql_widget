8e37aefea64195d6f948604e9e9cc8705dc762ed
Stefan Froemken <froemken@gmail.com>
2024-07-26 22:12:49 +0200

    Update TYPO3 dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,7 +1,7 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'MySQL Widget',
+    'title' => 'MySQL Dashboard Widget',
     'description' => 'Add DashBoard widgets to show MySQL status',
     'category' => 'plugin',
     'author' => 'Stefan Froemken',
@@ -9,11 +9,11 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => '',
     'state' => 'stable',
     'clearCacheOnLoad' => 0,
-    'version' => '0.2.1',
+    'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'php' => '7.4.0-8.1.99',
-            'typo3' => '10.4.32-11.5.99',
+            'php' => '8.1.0-8.3.99',
+            'typo3' => '11.5.37-12.4.99',
         ],
         'conflicts' => [
         ],
